b09b484f922ea467827169d4d4946bce8c413ff6
chosson <>
2020-10-19 12:23:08 -0400

    Removed answers.

--- exercice.py
+++ exercice.py
@@ -13,43 +13,20 @@ def build_note_dictionaries(note_names, add_octave_no=True):
 	midi_to_name = {}
 	name_to_midi = {}
 	# Pour chaque octave de 0 à 8 (inclus). On va générer tout l'octave 8, même si la dernière note du piano est Do 8
-	for octave in range(8+1):
 		# Pour chaque note de l'octave
-		for note in range(NOTES_PER_OCTAVE):
 			# Calculer le numéro MIDI de la note et ajouter aux deux dictionnaires
-			midi_no = C0_MIDI_NO + octave * NOTES_PER_OCTAVE + note
 			# Ajouter le numéro de l'octave au nom de la note si add_octave_no est vrai
-			full_note_name = note_names[note] + (str(octave) if add_octave_no else "")
-			midi_to_name[midi_no] = full_note_name
 			# Garder les numéros de notes dans name_to_midi entre 0 et 11 si add_octave_no est faux
-			name_to_midi[full_note_name] = midi_no if add_octave_no else midi_no % NOTES_PER_OCTAVE
 	return midi_to_name, name_to_midi
 
 def build_print_note_name_callback(midi_to_name):
-	def callback(midi_msg):
-		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
-			print(midi_to_name[midi_msg.note])
-	return callback
+	pass
 
 def build_print_chord_name_callback(chord_names_and_notes, name_to_midi):
-	chords = {}
-
-	for name, notes in chord_names_and_notes.items():
-		chord_notes = [False] * NOTES_PER_OCTAVE
-		for note in notes:
-			chord_notes[name_to_midi[note] % NOTES_PER_OCTAVE] = True
-		chords[tuple(chord_notes)] = name
-
-	note_states = [False] * NOTES_PER_OCTAVE
-	def callback(midi_msg):
-		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
-			note_states[midi_msg.note % NOTES_PER_OCTAVE] = True
-			note_states_key = tuple(note_states)
-			if note_states_key in chords:
-				print(chords[note_states_key])
-		elif midi_msg.type == "note_off" or (midi_msg.type == "note_on" and midi_msg.velocity == 0):
-			note_states[midi_msg.note % NOTES_PER_OCTAVE] = False
-	return callback
+	# Construire le dictionnaire d'assocations entre état des notes et accord joué.
+	
+	# Créez et retourner le callback
+	pass
 
 
 def main():
